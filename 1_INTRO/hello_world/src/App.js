b6f48ef57633197a44f0ad1b092676f35dfbf222
Antonio Filho <Antonio.Filho@fit-tecnologia.org.br>
2023-04-08 23:00:46 -0300

    Desafio_1, adicionar elemento no HTML e criar uma regra de estilo e mudar o titulo.

--- 1_INTRO/hello_world/src/App.js
+++ 1_INTRO/hello_world/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World React!</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello World React!</h1>
+            <h2>Meu primeiro parágrafo em React</h2>
+            <p>Emment funcionou!!!!!!</p>
+        </div>
+    );
 }
 
 export default App;
