14871c4d1b106b88335768f923b1a97ef36b0c2d
Antonio-Marquess <antonio.marques.pf@gmail.com>
2023-04-13 01:13:14 -0300

    Avançando em React

---
+++ 3_AVANCANDO_EM_REACT/rumo_ao_avancado/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Avançando em React</h1>
+        {/*Imagem em public*/}
+        <img src="/universo.jpg" alt="Universo" />
+    </div>
+  );
+}
+
+export default App;
