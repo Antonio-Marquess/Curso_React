8cdb96c78edf3cf19766bd2de43bf3e3448a10e1
Antonio Marques <antonio.marquess@outlook.com.br>
2023-04-25 15:48:41 -0300

    CSS inlene dinâmico

--- 4_CSS_REACT/css_react/src/App.js
+++ 4_CSS_REACT/css_react/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import MyComponent from './components/MyComponent';
+import { useState } from 'react';
 
 function App() {
+  const n = 15
+  const [name] = useState("Antonio")
+
   return (
     <div className="App">
         {/*CSS global*/}
@@ -13,6 +17,19 @@ function App() {
         <p style={{color: 'red', padding: '25px', borderTop: '2px solid red'}}>
             Parágrafo do App.js com CSS inline
         </p>
+        {/*CSS inlene dinâmico*/}
+          <h2 style={n < 10 ? ({ color: "purple" }) : ({ color: "pink" })}>
+              CSS Dinâmico</h2>
+          <h2 style={n > 10 ? ({ color: "purple" }) : ({ color: "pink" })}>
+              CSS Dinâmico</h2>
+          <h2 style={name === "Antonio" ? ({ color: "green", backgroundColor: "#000" })
+              : null
+          }
+          >
+              CSS Dinâmico, teste nome
+          </h2>
+             
+
     </div>
   );
 }
